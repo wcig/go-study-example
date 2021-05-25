964cbf326ddcfdc4db69cd16eeb8d4c8bc4d6f0b
wcig <job.yangbo@gmail.com>
2021-05-25 20:42:10 +0800

    go sdk: bufio

--- sdk/ch2_bufio/bufio_test.go
+++ sdk/ch2_bufio/bufio_test.go
@@ -30,6 +30,134 @@ func TestNewReaderSize(t *testing.T) {
 	assert.NotNil(t, reader)
 }
 
+// func (b *Reader) ReadSlice(delim byte) (line []byte, err error)
+// 读取直到输入中第一次出现delim为止，并返回一个指向缓冲区中字节的切片。字节在下一次读取时不再有效。(由于前一次读取会被后面覆盖,不建议使用)
+func TestReadSlice(t *testing.T) {
+	reader := bufio.NewReader(strings.NewReader("hello world.|It is ok"))
+
+	line1, _ := reader.ReadSlice('|')
+	fmt.Println("line1:", string(line1))
+
+	line2, _ := reader.ReadSlice('|')
+	fmt.Println("line2:", string(line2))
+	fmt.Println("line1:", string(line1))
+	// output:
+	// line1: hello world.|
+	// line2: It is ok
+	// line1: It is okrld.|
+}
+
+// func (b *Reader) ReadBytes(delim byte) ([]byte, error)
+// 读取直到输入中第一次出现delim为止，并返回一个切片，该切片包含直到定界符（包括定界符）的数据。(不会覆盖前一次读取,简单使用的话建议使用Scanner)
+func TestReadBytes(t *testing.T) {
+	reader := bufio.NewReader(strings.NewReader("hello world.|It is ok"))
+
+	line1, _ := reader.ReadBytes('|')
+	fmt.Println("line1:", string(line1))
+
+	line2, _ := reader.ReadBytes('|')
+	fmt.Println("line2:", string(line2))
+	fmt.Println("line1:", string(line1))
+	// output:
+	// line1: hello world.|
+	// line2: It is ok
+	// line1: hello world.|
+}
+
+// func (b *Reader) ReadString(delim byte) (string, error)
+// 读取直到输入中第一次出现delim为止，返回一个字符串，该字符串包含直到定界符（包括定界符）的数据。(不会覆盖前一次读取,简单使用的话建议使用Scanner)
+func TestReadString(t *testing.T) {
+	reader := bufio.NewReader(strings.NewReader("hello world.|It is ok"))
+
+	line1, _ := reader.ReadString('|')
+	fmt.Println("line1:", string(line1))
+
+	line2, _ := reader.ReadString('|')
+	fmt.Println("line2:", string(line2))
+	fmt.Println("line1:", string(line1))
+	// output:
+	// line1: hello world.|
+	// line2: It is ok
+	// line1: hello world.|
+}
+
+// func (b *Reader) ReadLine() (line []byte, isPrefix bool, err error)
+// 是一个低级别的行读取原语。 大多数调用者应改用ReadBytes('\n')或ReadString('\n')或使用扫描仪。
+func TestReadLine(t *testing.T) {
+	reader := bufio.NewReader(strings.NewReader("hello world.\nIt is ok"))
+	for {
+		line, isPrefix, err := reader.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Println(string(line), isPrefix)
+	}
+	// output:
+	// hello world. false
+	// It is ok false
+}
+
+// func (b *Reader) Read(p []byte) (n int, err error)
+// 读取数据并写入p,返回写入p的字节数和错误.可能存在n<len(p)情况,如需读取准确的len(p)需使用io.ReadFull(b,p)
+func TestRead(t *testing.T) {
+	reader := bufio.NewReader(strings.NewReader("hello world.|It is ok"))
+	buf := make([]byte, 100)
+	n, err := reader.Read(buf)
+	fmt.Println(n, err)           // 21 <nil>
+	fmt.Println(string(buf[0:n])) // hello world.|It is ok
+}
+
+// func (b *Reader) ReadByte() (byte, error)
+// 读取并返回一个字节,如果没有字节将返回错误 (建议使用bufio.Scanner更方便)
+func TestReadByte(t *testing.T) {
+	r := bufio.NewReader(strings.NewReader("hello"))
+	for {
+		b, err := r.ReadByte()
+		if err == io.EOF {
+			break
+		}
+		fmt.Println(string(b))
+	}
+	// output:
+	// h
+	// e
+	// l
+	// l
+	// o
+}
+
+// func (b *Reader) ReadRune() (r rune, size int, err error)
+// 读取单个UTF-8编码的Unicode字符，并返回符文及其大小（以字节为单位）。 (建议使用bufio.Scanner更方便)
+func TestReadRune(t *testing.T) {
+	r := bufio.NewReader(strings.NewReader("hello"))
+	for {
+		r, n, err := r.ReadRune()
+		if err == io.EOF {
+			break
+		}
+		fmt.Println(r, n)
+	}
+	// output:
+	// h
+	// e
+	// l
+	// l
+	// o
+}
+
+// func (b *Reader) WriteTo(w io.Writer) (n int64, err error)
+// 写入数据到w
+func TestWriteTo(t *testing.T) {
+	reader := bufio.NewReader(strings.NewReader("hello world.|It is ok"))
+	var buf bytes.Buffer
+	num, err := reader.WriteTo(&buf)
+	fmt.Println(num, err)     // 21 <nil>
+	fmt.Println(buf.String()) // hello world.|It is ok
+}
+
 // func NewWriter(w io.Writer) *Writer
 // 返回默认缓冲区大小的Writer (4096字节)
 func TestNewWriter(t *testing.T) {
@@ -46,85 +174,38 @@ func TestNewWriterSize(t *testing.T) {
 	fmt.Println(num, err) // 5 <nil>
 }
 
-// ReadSlice
-func TestReadSlice(t *testing.T) {
-	reader := bufio.NewReader(strings.NewReader("hello world.|It is ok"))
-
-	line1, _ := reader.ReadSlice('|')
-	fmt.Println("line1:", string(line1))
-
-	line2, _ := reader.ReadSlice('|')
-	fmt.Println("line2:", string(line2))
-	fmt.Println("line1:", string(line1))
-}
-
-// output:
-// line1: hello world.|
-// line2: It is ok
-// line1: It is okrld.|
-
-// ReadBytes
-func TestReadBytes(t *testing.T) {
-	reader := bufio.NewReader(strings.NewReader("hello world.|It is ok"))
-
-	line1, _ := reader.ReadBytes('|')
-	fmt.Println("line1:", string(line1))
-
-	line2, _ := reader.ReadBytes('|')
-	fmt.Println("line2:", string(line2))
-	fmt.Println("line1:", string(line1))
-}
-
-// output:
-// line1: hello world.|
-// line2: It is ok
-// line1: hello world.|
-
-// ReadString
-func TestReadString(t *testing.T) {
-	reader := bufio.NewReader(strings.NewReader("hello world.|It is ok"))
-
-	line1, _ := reader.ReadString('|')
-	fmt.Println("line1:", string(line1))
-
-	line2, _ := reader.ReadString('|')
-	fmt.Println("line2:", string(line2))
-	fmt.Println("line1:", string(line1))
-}
-
-// output:
-// line1: hello world.|
-// line2: It is ok
-// line1: hello world.|
-
-// ReadLine
-func TestReadLine(t *testing.T) {
-	reader := bufio.NewReader(strings.NewReader("hello world.\nIt is ok"))
-	for {
-		line, isPrefix, err := reader.ReadLine()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			t.Fatal(err)
-		}
-		fmt.Println(string(line), isPrefix)
-	}
-}
-
-// output:
-// hello world. false
-// It is ok false
-
-// WriteString
+// func (b *Writer) WriteString(s string) (int, error)
+// 写入一个字符串。 它返回写入的字节数。 如果计数小于len，它还会返回一个错误，解释为什么写时间短。
 func TestWriteString(t *testing.T) {
 	rb := &bytes.Buffer{}
 	writer := bufio.NewWriter(rb)
 	n, err := writer.WriteString("hello world.")
-	fmt.Println(n, err) // 12 <nil>
+	fmt.Println(n, err)      // 12 <nil>
+	fmt.Println(rb.String()) //
+}
 
-	writer.Flush()
+// func (b *Writer) Flush() error
+// 缓冲区数据写入底层io.Writer中
+func TestFlush(t *testing.T) {
+	rb := &bytes.Buffer{}
+	writer := bufio.NewWriter(rb)
+	_, err := writer.WriteString("hello world.")
+	assert.Nil(t, err)
+
+	err = writer.Flush()
+	assert.Nil(t, err)
 	fmt.Println(rb.String()) // hello world.
+}
+
+// func NewReadWriter(r *Reader, w *Writer) *ReadWriter
+// 分配一个新的ReadWriter，该ReadWriter调度到r和w。(使用较少)
+func TestNewReadWriter(t *testing.T) {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	readWriter := bufio.NewReadWriter(reader, writer)
+	n, err := readWriter.WriteString("hello")
+	fmt.Println(n, err)
+	_ = readWriter.Flush()
 }
 
 // Test for golang.org/issue/5947
@@ -148,6 +229,9 @@ func TestWriterReadFromWhileFull(t *testing.T) {
 	fmt.Println(buf.String()) // 0123456789abcdef
 }
 
+// func NewScanner(r io.Reader) *Scanner
+// 返回一个新的Scanner以从r读取。 拆分功能默认为ScanLines。
+// 分割函数bufio.ScanLines: 按换行符'\n'分割
 func TestScannerWithScanLines(t *testing.T) {
 	reader := strings.NewReader("hello world.\nit is ok\n666")
 	scanner := bufio.NewScanner(reader)
@@ -156,13 +240,13 @@ func TestScannerWithScanLines(t *testing.T) {
 		text := scanner.Text()
 		fmt.Println(text)
 	}
+	// output:
+	// hello world.
+	// it is ok
+	// 666
 }
 
-// output:
-// hello world.
-// it is ok
-// 666
-
+// 分割函数bufio.ScanWords: 按单词分割
 func TestScannerWithScanWords(t *testing.T) {
 	reader := strings.NewReader("hello world. 666")
 	scanner := bufio.NewScanner(reader)
@@ -172,13 +256,13 @@ func TestScannerWithScanWords(t *testing.T) {
 		text := scanner.Text()
 		fmt.Println(text)
 	}
+	// output:
+	// hello
+	// world.
+	// 666
 }
 
-// output:
-// hello
-// world.
-// 666
-
+// 分割函数bufio.ScanBytes: 按字节分割
 func TestScannerWithScanBytes(t *testing.T) {
 	reader := strings.NewReader("hello")
 	scanner := bufio.NewScanner(reader)
@@ -188,15 +272,15 @@ func TestScannerWithScanBytes(t *testing.T) {
 		text := scanner.Text()
 		fmt.Println(text)
 	}
+	// output:
+	// h
+	// e
+	// l
+	// l
+	// o
 }
 
-// output:
-// h
-// e
-// l
-// l
-// o
-
+// 分割函数bufio.ScanRunes: 按字符分割
 func TestScannerWithScanRunes(t *testing.T) {
 	reader := strings.NewReader("天气ok")
 	scanner := bufio.NewScanner(reader)
@@ -206,10 +290,9 @@ func TestScannerWithScanRunes(t *testing.T) {
 		text := scanner.Text()
 		fmt.Println(text)
 	}
+	// output:
+	// 天
+	// 气
+	// o
+	// k
 }
-
-// output:
-// 天
-// 气
-// o
-// k
