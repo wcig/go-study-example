d4ba5ddfecc13ab176be747b9554242a01ad538f
wcig <job.yangbo@gmail.com>
2021-06-03 20:42:03 +0800

    go sdk: imagemagick

--- third/ch1_image/imagemagick/imagemagick_test.go
+++ third/ch1_image/imagemagick/imagemagick_test.go
@@ -10,15 +10,29 @@ import (
 // imagemagick
 // version: 6.9.7, module: gopkg.in/gographics/imagick.v2 v2.6.0
 
-func TestAdaptiveResizeImage(t *testing.T) {
-	var err error
-
+func TestGetImageSize(t *testing.T) {
 	imagick.Initialize()
 	defer imagick.Terminate()
 
 	mw := imagick.NewMagickWand()
 	defer mw.Destroy()
 
+	var err error
+	if err = mw.ReadImage("src.jpeg"); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("image w:%d, h:%d\n", mw.GetImageWidth(), mw.GetImageHeight()) // image w:360, h:640
+}
+
+func TestAdaptiveResizeImage(t *testing.T) {
+	imagick.Initialize()
+	defer imagick.Terminate()
+
+	mw := imagick.NewMagickWand()
+	defer mw.Destroy()
+
+	var err error
 	if err = mw.ReadImage("src.jpeg"); err != nil {
 		panic(err)
 	}
