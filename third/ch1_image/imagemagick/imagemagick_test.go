1ad2380e56d5dde3c81c89f77d9357a4136e9c00
wcig <job.yangbo@gmail.com>
2021-06-03 20:07:28 +0800

    go sdk: imagemagick

--- third/ch1_image/imagemagick/imagemagick_test.go
+++ third/ch1_image/imagemagick/imagemagick_test.go
@@ -7,16 +7,35 @@ import (
 	"gopkg.in/gographics/imagick.v2/imagick"
 )
 
-func Test(t *testing.T) {
-	// imagick.Initialize()
-	// defer imagick.Terminate()
+// imagemagick
+// version: 6.9.7, module: gopkg.in/gographics/imagick.v2 v2.6.0
 
-	// mw := imagick.NewMagickWand()
-	// mw.Destroy()
+func TestAdaptiveResizeImage(t *testing.T) {
+	var err error
 
+	imagick.Initialize()
+	defer imagick.Terminate()
+
+	mw := imagick.NewMagickWand()
+	defer mw.Destroy()
+
+	if err = mw.ReadImage("src.jpeg"); err != nil {
+		panic(err)
+	}
+
+	if err = mw.AdaptiveResizeImage(200, 200); err != nil {
+		panic(err)
+	}
+
+	if err = mw.WriteImage("tmp.200x200.jpeg"); err != nil {
+		panic(err)
+	}
+}
+
+func TestConvertCmd(t *testing.T) {
 	imageCommandResult, err := imagick.ConvertImageCommand([]string{
-		"convert", "/Users/yangbo/imagemagick/test.png", "-resize", "200x200", "/Users/yangbo/imagemagick/test-out.png",
+		"convert", "src.jpeg", "-resize", "200x200", "tmp.200x200.jpeg",
 	})
 	fmt.Println(err)                // <nil>
-	fmt.Println(imageCommandResult) // &{0xc000010040 200,200,PNG}
+	fmt.Println(imageCommandResult) // &{0xc000010040 113,200,JPEG}
 }
