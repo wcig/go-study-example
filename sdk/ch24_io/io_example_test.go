f613d22a1d0eff37ff4958a423e306ccb861a0b2
wcig <job.yangbo@gmail.com>
2021-06-21 20:44:04 +0800

    go sdk: io

--- sdk/ch24_io/io_example_test.go
+++ sdk/ch24_io/io_example_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"strings"
 	"testing"
@@ -11,7 +12,174 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// io：io的基本接口。
+// Copy
+func TestCopy(t *testing.T) {
+	rb := &bytes.Buffer{}
+	wb := &bytes.Buffer{}
+
+	rb.WriteString("hello, world.")
+	written, err := io.Copy(wb, rb)
+	if err != nil || written != 13 {
+		t.Fatal(err)
+	}
+	assert.True(t, wb.String() == "hello, world.")
+}
+
+// CopyBuffer
+func TestCopyBuffer(t *testing.T) {
+	r1 := strings.NewReader("first reader\n")
+	r2 := strings.NewReader("second reader\n")
+	buf := make([]byte, 8)
+
+	// buf is used here...
+	if _, err := io.CopyBuffer(os.Stdout, r1, buf); err != nil {
+		log.Fatal(err)
+	}
+
+	// ... reused here also. No need to allocate an extra buffer.
+	if _, err := io.CopyBuffer(os.Stdout, r2, buf); err != nil {
+		log.Fatal(err)
+	}
+	// output:
+	// first reader
+	// second reader
+}
+
+// CopyN
+func TestCopyN(t *testing.T) {
+	rb := &bytes.Buffer{}
+	wb := &bytes.Buffer{}
+
+	rb.WriteString("hello, world.")
+	written, err := io.CopyN(wb, rb, 5)
+	if err != nil || written != 5 {
+		t.Fatal(err)
+	}
+	assert.True(t, wb.String() == "hello")
+}
+
+// Pipe
+func TestPipe(t *testing.T) {
+	r, w := io.Pipe()
+
+	go func() {
+		n, err := w.Write([]byte("hello world.\n"))
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("成功写入字节数:", n)
+		_ = w.Close()
+	}()
+
+	n, err := io.Copy(os.Stdout, r)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("成功读取字节数:", n)
+	// output:
+	// hello world.
+	// 成功写入字节数: 13
+	// 成功读取字节数: 13
+}
+
+// ReadAll
+func TestReadAll(t *testing.T) {
+	src := "hello world."
+	b, err := io.ReadAll(strings.NewReader(src))
+	assert.Nil(t, err)
+	assert.Equal(t, src, string(b))
+}
+
+// ReadAtLeast
+func TestReadAtLeast(t *testing.T) {
+	r := strings.NewReader("some io.Reader stream to be read\n")
+
+	buf := make([]byte, 14)
+	n, err := io.ReadAtLeast(r, buf, 4)
+	assert.Nil(t, err)
+	fmt.Println(n, string(buf))
+
+	shortBuf := make([]byte, 3)
+	_, err = io.ReadAtLeast(r, shortBuf, 4)
+	fmt.Println(err == io.ErrShortBuffer, err)
+
+	longBuf := make([]byte, 64)
+	_, err = io.ReadAtLeast(r, longBuf, 64)
+	fmt.Println(err == io.ErrUnexpectedEOF, err)
+	// output:
+	// 14 some io.Reader
+	// true short buffer
+	// true unexpected EOF
+}
+
+// ReadFull
+func TestReadFull(t *testing.T) {
+	r := strings.NewReader("some io.Reader stream to be read\n")
+
+	buf := make([]byte, 4)
+	n, err := io.ReadFull(r, buf)
+	assert.Nil(t, err)
+	fmt.Println(n, string(buf))
+
+	longBuf := make([]byte, 64)
+	_, err = io.ReadFull(r, longBuf)
+	fmt.Println(err == io.ErrUnexpectedEOF, err)
+	// output:
+	// 4 some
+	// true unexpected EOF
+}
+
+// WriteString
+func TestWriteString(t *testing.T) {
+	n, err := io.WriteString(os.Stdout, "hello world.\n")
+	assert.Nil(t, err)
+	fmt.Println(n)
+	// output:
+	// hello world.
+	// 13
+}
+
+// ByteReader接口
+func TestByteReader(t *testing.T) {
+	buf := bytes.NewBuffer([]byte("hello world."))
+	b, err := buf.ReadByte()
+	assert.Nil(t, err)
+	fmt.Printf("%q\n", b) // 'h'
+}
+
+// ByteScanner接口
+func TestByteScanner(t *testing.T) {
+	buf := bytes.NewBuffer([]byte("hello world."))
+	b, err := buf.ReadByte()
+	assert.Nil(t, err)
+	fmt.Printf("%q\n", b)
+
+	err = buf.UnreadByte()
+	assert.Nil(t, err)
+
+	b, err = buf.ReadByte()
+	assert.Nil(t, err)
+	fmt.Printf("%q\n", b)
+	// output:
+	// 'h'
+	// 'h'
+}
+
+// ByteWriter接口
+func TestByteWriter(t *testing.T) {
+	buf := bytes.NewBuffer(nil)
+	err := buf.WriteByte('h')
+	assert.Nil(t, err)
+	fmt.Println(buf.String()) // h
+}
+
+// Closer接口
+func TestCloser(t *testing.T) {
+	file, err := os.Open("io_example_test.go")
+	assert.Nil(t, err)
+	err = file.Close()
+	assert.Nil(t, err)
+}
 
 // ReaderFrom接口
 func TestReaderFrom(t *testing.T) {
@@ -67,44 +235,6 @@ func TestWriterAt(t *testing.T) {
 	}
 	fmt.Println(n) // 3
 	// temp3: ok-hello666rld.
-}
-
-// Copy
-func TestCopy(t *testing.T) {
-	rb := &bytes.Buffer{}
-	wb := &bytes.Buffer{}
-
-	rb.WriteString("hello, world.")
-	written, err := io.Copy(wb, rb)
-	if err != nil || written != 13 {
-		t.Fatal(err)
-	}
-	assert.True(t, wb.String() == "hello, world.")
-}
-
-// CopyN
-func TestCopyN(t *testing.T) {
-	rb := &bytes.Buffer{}
-	wb := &bytes.Buffer{}
-
-	rb.WriteString("hello, world.")
-	written, err := io.CopyN(wb, rb, 5)
-	if err != nil || written != 5 {
-		t.Fatal(err)
-	}
-	assert.True(t, wb.String() == "hello")
-}
-
-// CopyBuffer
-func TestCopyBuffer(t *testing.T) {
-	rb := &bytes.Buffer{}
-	wb := &bytes.Buffer{}
-
-	rb.WriteString("hello, world.")
-	io.CopyBuffer(wb, rb, make([]byte, 1))
-	if wb.String() != "hello, world." {
-		t.Errorf("CopyBuffer did not work properly")
-	}
 }
 
 // MultiReader
--- sdk/ch24_io/io_test.go
+++ sdk/ch24_io/io_test.go
@@ -23,3 +23,32 @@ func TestErr(t *testing.T) {
 	_ = io.ErrShortWrite
 	_ = io.ErrUnexpectedEOF
 }
+
+// 函数
+// func Copy(dst Writer, src Reader) (written int64, err error)                   // 从src拷贝数据至dst,直至遇到EOF或错误
+// func CopyBuffer(dst Writer, src Reader, buf []byte) (written int64, err error) // 与Copy类似,区别在使用指定的buf而不是分配的临时缓冲区
+// func CopyN(dst Writer, src Reader, n int64) (written int64, err error) // 从src拷贝n个字节到dst
+// func Pipe() (*PipeReader, *PipeWriter) // 创建一同步的内存管道,用于连接io.Reader和io.Writer
+// func ReadAll(r Reader) ([]byte, error) // 读取r所有数据直至遇到EOF或错误,成功返回err=nil
+// func ReadAtLeast(r Reader, buf []byte, min int) (n int, err error) // 从r读取至少min个字节到buf,超过min则读取最多len(buf)个字节
+// func ReadFull(r Reader, buf []byte) (n int, err error) // 从r读取准备的len(buf)个字节
+// func WriteString(w Writer, s string) (n int, err error) // 往w写入字符串s
+
+// 接口
+// 1.type ByteReader: 包装唯一ReadByte方法接口
+// type ByteReader interface {
+//    ReadByte() (byte, error)
+// }
+
+// 2.type ByteScanner: ByteReader接口基础上增加一UnreadByte方法 (UnreadByte会导致下一次ReadByte和上一次返回同一个byte,两次UnreadByte之间没有ReadByte调用将返回错误)
+// type ByteScanner interface {
+//    ByteReader
+//    UnreadByte() error
+// }
+
+// 3.type ByteWriter: 包装唯一的WriteByte方法接口
+// type ByteWriter interface {
+//    WriteByte(c byte) error
+// }
+
+// 4.type Closer: 包装唯一Close方法接口
