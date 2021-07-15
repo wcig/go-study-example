ac41b9b8c92d9d930fb1c68c03f1fb357ea985ac
wcig <job.yangbo@gmail.com>
2021-07-15 20:41:02 +0800

    go sdk: os/exec

--- sdk/ch29_os/exec/exec_util_test.go
+++ sdk/ch29_os/exec/exec_util_test.go
@@ -2,9 +2,11 @@ package exec
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"os/exec"
 	"testing"
+	"time"
 )
 
 func TestRunCmd(t *testing.T) {
@@ -16,10 +18,44 @@ func TestRunCmd(t *testing.T) {
 	fmt.Println("output:", output) // output: ok\n
 }
 
+func TestRunCmdWithTimeout(t *testing.T) {
+	{
+		cmdStr := "sleep 3"
+		_, err := RunCmdWithTimeout(cmdStr, 5*time.Second)
+		if err != nil {
+			panic(err)
+		}
+	}
+	{
+		cmdStr := "sleep 3"
+		_, err := RunCmdWithTimeout(cmdStr, 1*time.Second)
+		if err == nil {
+			panic("cmd run no timeout err")
+		}
+	}
+
+}
+
 // ------------------------------------------------------------------ //
 
 func RunCmd(cmdStr string) (output string, err error) {
 	cmd := exec.Command("/bin/sh", "-c", cmdStr)
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	if err = cmd.Run(); err != nil {
+		err = fmt.Errorf("cmd: %s, stdout: %s, stderr: %s, err: %v", cmd, stdout.String(), stderr.String(), err)
+		return stdout.String(), err
+	}
+	return stdout.String(), nil
+}
+
+func RunCmdWithTimeout(cmdStr string, timeout time.Duration) (output string, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	cmd := exec.CommandContext(ctx, "/bin/sh", "-c", cmdStr)
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
