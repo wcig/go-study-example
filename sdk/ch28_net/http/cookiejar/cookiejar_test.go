86d247a70c546fd132022977ece8fe1a5807edb1
wcig <job.yangbo@gmail.com>
2021-09-20 22:38:33 +0800

    go sdk: net/http/cookiejar

---
+++ sdk/ch28_net/http/cookiejar/cookiejar_test.go
@@ -0,0 +1,11 @@
+package cookiejar
+
+// net/http/cookiejar: 实现了内存的符合RFC 6265的http.CookieJar
+
+// 函数
+// type Jar
+//    func New(o *Options) (*Jar, error)
+//    func (j *Jar) Cookies(u *url.URL) (cookies []*http.Cookie)
+//    func (j *Jar) SetCookies(u *url.URL, cookies []*http.Cookie)
+// type Options
+// type PublicSuffixList
