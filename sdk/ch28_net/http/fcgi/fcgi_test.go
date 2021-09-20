b83446b620bef5c4d2535b99d2f65492483f3852
wcig <job.yangbo@gmail.com>
2021-09-20 22:36:42 +0800

    go sdk: net/http/fcgi

---
+++ sdk/ch28_net/http/fcgi/fcgi_test.go
@@ -0,0 +1,11 @@
+package fcgi
+
+// net/http/fcgi: 实现了FastCGI协议
+
+// 变量
+// var ErrConnClosed = errors.New("fcgi: connection to web server closed")
+// var ErrRequestAborted = errors.New("fcgi: request aborted by web server")
+
+// 函数
+// func ProcessEnv(r *http.Request) map[string]string
+// func Serve(l net.Listener, handler http.Handler) error
