3923381a578c5dfb3ca25d6f37d169aa890c2fa5
wcig <job.yangbo@gmail.com>
2022-01-04 22:02:20 +0800

    go study: downloader

--- study/downloader/downloader.go
+++ study/downloader/downloader.go
@@ -1,3 +1,161 @@
 package downloader
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"sync"
+)
+
 // 支持并发下载的下载器（参考：https://polarisxu.studygolang.com/posts/go/action/build-a-concurrent-file-downloader/）
+
+type Downloader struct {
+	concurrency          int
+	fileUrl              string
+	fileName             string
+	fileSize             int64
+	supportMultiDownload bool
+	tmpDir               string
+}
+
+func NewDownloader(concurrency int) *Downloader {
+	return &Downloader{concurrency: concurrency}
+}
+
+func (d *Downloader) Download(fileUrl string, fileName string) error {
+	urlInfo, err := url.Parse(fileUrl)
+	if err != nil {
+		return err
+	}
+
+	d.fileUrl = fileUrl
+	d.fileName = fileName
+	if d.fileName == "" {
+		d.fileName = filepath.Base(urlInfo.Path)
+	}
+
+	if err = d.isSupportMultiDownload(); err != nil {
+		return err
+	}
+	if !d.supportMultiDownload {
+		return d.singleDownload()
+	} else {
+		return d.multiDownload()
+	}
+}
+
+func (d *Downloader) isSupportMultiDownload() error {
+	resp, err := http.Head(d.fileUrl)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode == http.StatusOK && resp.Header.Get("Accept-Ranges") == "bytes" {
+		d.supportMultiDownload = true
+		d.fileSize = resp.ContentLength
+	}
+	return nil
+}
+
+func (d *Downloader) singleDownload() error {
+	resp, err := http.Get(d.fileUrl)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	file, err := os.Create(d.fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, resp.Body)
+	return err
+}
+
+func (d *Downloader) multiDownload() error {
+	dir, err := os.MkdirTemp("./", "frag-")
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(dir)
+	d.tmpDir = dir
+
+	var wg sync.WaitGroup
+	wg.Add(d.concurrency)
+	fragSize := d.fileSize / int64(d.concurrency)
+	for i := 1; i <= d.concurrency; i++ {
+		go func(i int, fragSize int64) {
+			defer wg.Done()
+			start := int64(i-1) * fragSize
+			end := int64(i)*fragSize - 1
+			if i == d.concurrency {
+				end = d.fileSize
+			}
+			if err2 := d.downloadFrag(i, start, end); err2 != nil {
+				err = err2
+			}
+			fmt.Println("download frag over:", i, start, end)
+		}(i, fragSize)
+	}
+	if err != nil {
+		return err
+	}
+
+	wg.Wait()
+	return d.mergeFrag()
+}
+
+func (d *Downloader) downloadFrag(i int, start int64, end int64) error {
+	fmt.Println("download frag:", i, start, end)
+
+	req, err := http.NewRequest("GET", d.fileUrl, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Range", fmt.Sprintf("bytes=%d-%d", start, end))
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	fragFileName := d.getFragFileName(i)
+	file, err := os.Create(fragFileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, resp.Body)
+	return err
+}
+
+func (d *Downloader) mergeFrag() error {
+	fmt.Println("merge frag")
+	file, err := os.Create(d.fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	for i := 1; i <= d.concurrency; i++ {
+		fragFileName := d.getFragFileName(i)
+		fragFile, err := os.Open(fragFileName)
+		if err != nil {
+			return err
+		}
+		defer fragFile.Close()
+		if _, err = io.Copy(file, fragFile); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (d *Downloader) getFragFileName(i int) string {
+	return filepath.Join(d.tmpDir, fmt.Sprintf("frag-%d", i))
+}
--- study/downloader/downloader_test.go
+++ study/downloader/downloader_test.go
@@ -1,10 +1,13 @@
 package downloader
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,26 +17,26 @@ func TestFirst(t *testing.T) {
 	fileUrl := "https://dlcdn.apache.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.tar.gz"
 	fileName := filepath.Base(fileUrl)
 
-	isSupport, err := checkSupportMultiDownload(fileUrl)
+	isSupport, fileSize, err := checkSupportMultiDownload(fileUrl)
 	assert.Nil(t, err)
 
 	if isSupport {
-		err = multiDownload(fileUrl, fileName)
+		err = multiDownload(fileUrl, fileSize, fileName)
 	} else {
 		err = singleDownload(fileUrl, fileName)
 	}
 	assert.Nil(t, err)
 }
 
-func checkSupportMultiDownload(url string) (ok bool, err error) {
+func checkSupportMultiDownload(url string) (ok bool, fileSize int64, err error) {
 	resp, err := http.Head(url)
 	if err != nil {
-		return false, err
+		return false, 0, err
 	}
 	if resp.StatusCode == http.StatusOK && resp.Header.Get("Accept-Ranges") == "bytes" {
-		return true, nil
+		return true, resp.ContentLength, nil
 	}
-	return false, nil
+	return false, 0, nil
 }
 
 func singleDownload(fileUrl string, fileName string) error {
@@ -53,8 +56,94 @@ func singleDownload(fileUrl string, fileName string) error {
 	return err
 }
 
-func multiDownload(fileUrl string, fileName string) error {
-	// 1.get file content length by head request
-	// 2.send multi download request
-	// 3.merge multi file
+func multiDownload(fileUrl string, fileSize int64, fileName string) error {
+	dir, err := os.MkdirTemp("./", "frag-")
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(dir)
+
+	const fragSize int64 = 512 * 1024 // byte
+	num := int(fileSize / fragSize)
+	if fileSize%fragSize != 0 {
+		num++
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(num)
+	for i := 1; i <= num; i++ {
+		start := int64(i-1) * fragSize
+		end := int64(i)*fragSize - 1
+		if end > fileSize {
+			end = fileSize
+		}
+		n := i
+		go func() {
+			defer wg.Done()
+			if err = downloadFrag(fileUrl, dir, n, start, end); err != nil {
+				panic(err)
+			}
+			fmt.Printf("download frag %d over\n", n)
+		}()
+	}
+
+	wg.Wait()
+	return mergeFrag(fileName, dir, num)
+	// return nil
+}
+
+func downloadFrag(fileUrl string, dir string, index int, start int64, end int64) error {
+	fmt.Println("download frag:", fileUrl, dir, index, start, end)
+
+	req, err := http.NewRequest("GET", fileUrl, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Range", fmt.Sprintf("bytes=%d-%d", start, end))
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	file, err := os.Create(filepath.Join(dir, strconv.Itoa(index)))
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, resp.Body)
+	return err
+}
+
+func mergeFrag(fileName string, dir string, num int) error {
+	fmt.Println("merge frag:", fileName, dir, num)
+
+	file, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	for i := 1; i <= num; i++ {
+		fragFileName := filepath.Join(dir, strconv.Itoa(i))
+		fragFile, err := os.Open(fragFileName)
+		if err != nil {
+			return err
+		}
+		defer fragFile.Close()
+
+		if _, err = io.Copy(file, fragFile); err != nil {
+			return err
+
+		}
+	}
+	return nil
+}
+
+func TestDownloaderMultiDownload(t *testing.T) {
+	dl := NewDownloader(5)
+	fileUrl := "https://dlcdn.apache.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.tar.gz"
+	err := dl.Download(fileUrl, "")
+	assert.Nil(t, err)
 }
