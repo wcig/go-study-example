dcbf2ba15c6e72115646747263b3fe6c0e517bd7
wcig <job.yangbo@gmail.com>
2022-03-09 23:55:01 +0800

    go algorithm: simple hash table

--- algorithm/hashtable/simplehashtable/simplehashtable.go
+++ algorithm/hashtable/simplehashtable/simplehashtable.go
@@ -6,52 +6,130 @@ const (
 )
 
 type SimpleHashTable struct {
-	table []*Element
-	hash  Hash
-	size  int
-	cap   int
+	table    []*Element
+	hashFunc HashFunc
+	size     int
+	cap      int
 }
 
 type Element struct {
+	hash  int
 	value interface{}
 	next  *Element
 }
 
-type Hash func(v interface{}, tableSize int) int
+type HashFunc func(v interface{}) int
 
-func New(hash Hash) *SimpleHashTable {
+func New(hashFunc HashFunc) *SimpleHashTable {
 	return &SimpleHashTable{
-		table: make([]*Element, defaultCapacity, defaultCapacity),
-		hash:  hash,
-		size:  0,
-		cap:   defaultCapacity,
+		table:    make([]*Element, defaultCapacity, defaultCapacity),
+		hashFunc: hashFunc,
+		size:     0,
+		cap:      defaultCapacity,
 	}
 }
 
+// todo resize
 func (t *SimpleHashTable) Add(v interface{}) {
-	ae := &Element{
+	hash := t.hashFunc(v)
+	newElement := &Element{
+		hash:  hash,
 		value: v,
 		next:  nil,
 	}
 
-	hash := t.hash(v, t.cap)
-	e := t.table[hash]
+	index := t.getKeyTableIndex(hash)
+	e := t.table[index]
 	if e == nil {
-		t.table[hash] = ae
+		t.table[index] = newElement
+		t.size++
 		return
 	}
-
 	for {
+		if e != nil && e.hash == newElement.hash && e.value == newElement.value {
+			return
+		}
+		if e.next == nil {
+			break
+		}
 		e = e.next
+	}
+	e.next = newElement
+	t.size++
+}
+
+func (t *SimpleHashTable) Contain(v interface{}) bool {
+	return t.getElement(t.hash(v), v) != nil
+}
+
+func (t *SimpleHashTable) Remove(v interface{}) bool {
+	if t.size <= 0 {
+		return false
+	}
+
+	hash := t.hash(v)
+	index := t.getKeyTableIndex(hash)
+	first := t.table[index]
+	if first == nil {
+		return false
+	}
+	var beforeElement *Element
+	e := first
+	var exist bool
+	for e != nil {
+		if e.hash == hash && e.value == v {
+			exist = true
+			break
+		}
+		beforeElement = e
+		e = e.next
+	}
+	if exist {
+		if beforeElement == nil {
+			t.table[index] = e.next
+		} else {
+			beforeElement.next = e.next
+		}
+	}
+	t.size--
+	return true
+}
+
+func (t *SimpleHashTable) Size() int {
+	return t.size
+}
+
+func (t *SimpleHashTable) IsEmpty() bool {
+	return t.size == 0
+}
+
+func (t *SimpleHashTable) Clear() {
+	if t.size > 0 {
+		for i := 0; i < len(t.table); i++ {
+			t.table[i] = nil
+		}
+		t.size = 0
 	}
 }
 
-func (t *SimpleHashTable) Contain(v interface{}) bool { return false }
+func (t *SimpleHashTable) getElement(hash int, v interface{}) *Element {
+	if t.size <= 0 {
+		return nil
+	}
 
-func (t *SimpleHashTable) Remove(v interface{}) {}
+	index := t.getKeyTableIndex(hash)
+	for e := t.table[index]; e != nil; e = e.next {
+		if e.hash == hash && e.value == v {
+			return e
+		}
+	}
+	return nil
+}
 
-func (t *SimpleHashTable) Size() int { return 0 }
+func (t *SimpleHashTable) hash(v interface{}) int {
+	return t.hashFunc(v)
+}
 
-func (t *SimpleHashTable) IsEmpty() bool { return false }
-
-func (t *SimpleHashTable) Clear() {}
+func (t *SimpleHashTable) getKeyTableIndex(hash int) int {
+	return (t.cap - 1) & hash // 等价于: hash%t.cap
+}
---
+++ algorithm/hashtable/simplehashtable/simplehashtable_test.go
@@ -0,0 +1,84 @@
+package simplehashtable
+
+import (
+	"fmt"
+	"testing"
+)
+
+type user struct {
+	Id   int
+	Name string
+}
+
+func userHash(v interface{}) int {
+	intHash := func(i int) int {
+		return i
+	}
+	stringHash := func(s string) int {
+		h := 0
+		if s != "" {
+			for _, r := range []rune(s) {
+				h += 31*h + int(r)
+			}
+		}
+		return h
+	}
+	u := v.(user)
+	h := 1
+	h += 31*h + intHash(u.Id)
+	h += 31*h + stringHash(u.Name)
+	return h
+}
+
+func TestSimple(t *testing.T) {
+	u := user{1, "tom"}
+	fmt.Println("user hash:", userHash(u)) // user hash: 123501
+
+	ht := New(userHash)
+	fmt.Printf("after init, size: %d, empty: %t\n", ht.size, ht.IsEmpty())
+
+	ht.Add(u)
+	fmt.Printf("after add, size: %d, empty: %t, contain: %t\n", ht.size, ht.IsEmpty(), ht.Contain(u))
+
+	ht.Remove(u)
+	fmt.Printf("after remove, size: %d, empty: %t, contain: %t\n", ht.size, ht.IsEmpty(), ht.Contain(u))
+
+	_ = ht
+}
+
+func TestAdd(t *testing.T) {
+	u1 := user{1, "tom"}
+	u2 := user{1, "to}"}
+	u3 := user{1, "to]"}
+	fmt.Println(userHash(u1)%16, userHash(u2)%16, userHash(u3)%16)
+
+	ht := New(userHash)
+	ht.Add(u1)
+	fmt.Printf("add u1, size: %d, empty: %t, contain: %t\n", ht.size, ht.IsEmpty(), ht.Contain(u1))
+	ht.Add(u2)
+	fmt.Printf("add u2, size: %d, empty: %t, contain: %t\n", ht.size, ht.IsEmpty(), ht.Contain(u2))
+	ht.Add(u3)
+	fmt.Printf("add u3, size: %d, empty: %t, contain: %t\n", ht.size, ht.IsEmpty(), ht.Contain(u3))
+}
+
+func TestRemove(t *testing.T) {
+	u1 := user{1, "tom"}
+	u2 := user{1, "to}"}
+	u3 := user{1, "to]"}
+	fmt.Println(userHash(u1)%16, userHash(u2)%16, userHash(u3)%16)
+
+	ht := New(userHash)
+	ht.Add(u1)
+	fmt.Printf("add u1, size: %d, empty: %t, contain: %t\n", ht.size, ht.IsEmpty(), ht.Contain(u1))
+	ht.Add(u2)
+	fmt.Printf("add u2, size: %d, empty: %t, contain: %t\n", ht.size, ht.IsEmpty(), ht.Contain(u2))
+	ht.Add(u3)
+	fmt.Printf("add u3, size: %d, empty: %t, contain: %t\n", ht.size, ht.IsEmpty(), ht.Contain(u3))
+
+	ht.Remove(u1)
+	fmt.Printf("remove u1, size: %d, empty: %t, contain: %t\n", ht.size, ht.IsEmpty(), ht.Contain(u1))
+	ht.Remove(u2)
+	fmt.Printf("remove u2, size: %d, empty: %t, contain: %t\n", ht.size, ht.IsEmpty(), ht.Contain(u2))
+	ht.Remove(u3)
+	fmt.Printf("remove u3, size: %d, empty: %t, contain: %t\n", ht.size, ht.IsEmpty(), ht.Contain(u3))
+}
