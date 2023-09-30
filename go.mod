cb38d8dc3dd5c2de505e2fe69e66eba527824e9c
wcig <job.yangbo@gmail.com>
2023-09-30 12:41:23 +0800

    third: mongodb

--- go.mod
+++ go.mod
@@ -67,12 +67,13 @@ require (
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/go-zeromq/goczmq/v4 v4.2.2 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
-	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
+	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/btree v1.0.0 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
@@ -86,6 +87,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/mschoch/smat v0.2.0 // indirect
 	github.com/onsi/ginkgo v1.14.2 // indirect
 	github.com/onsi/gomega v1.10.4 // indirect
@@ -112,8 +114,13 @@ require (
 	github.com/syndtr/goleveldb v1.0.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/xuri/efp v0.0.0-20220407160117-ad0f7a785be8 // indirect
 	github.com/xuri/nfp v0.0.0-20220409054826-5e722a1d9e22 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.mongodb.org/mongo-driver v1.12.1 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/arch v0.3.0 // indirect
---
+++ third/ch102_mongodb/mongo_test.go
@@ -0,0 +1,157 @@
+package ch102_mongodb
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"testing"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	"go.mongodb.org/mongo-driver/bson"
+
+	"go.mongodb.org/mongo-driver/event"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+// 参考: https://www.mongodb.com/blog/post/mongodb-go-driver-tutorial
+// mongo操作对象:
+// bson.M: map[string]interface{}
+// bson.A: []interface{}
+// bson.E: struct{Key string, Value interface{}}
+// bson.D: []bson.E
+
+const (
+	logFormat     = "[mongo] [info]  %s [CMD] %s\n"
+	logTimeFormat = "2006/01/02 15:04:05.999999"
+)
+
+var (
+	mc *mongo.Client
+)
+
+func initMongo() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	cmdMonitor := &event.CommandMonitor{
+		Started: func(_ context.Context, e *event.CommandStartedEvent) {
+			fmt.Printf(logFormat, time.Now().Format(logTimeFormat), e.Command)
+		},
+	}
+	opt := options.Client().ApplyURI("mongodb://root:123456@localhost:27017").
+		SetMonitor(cmdMonitor).
+		SetMaxPoolSize(30).
+		SetMaxConnIdleTime(time.Minute)
+	client, err := mongo.Connect(ctx, opt)
+	if err != nil {
+		log.Fatalf("mongo connect error: %v", err)
+	}
+
+	if err = client.Ping(context.Background(), readpref.Primary()); err != nil {
+		log.Fatalf("mongo ping error: %v", err)
+	}
+
+	mc = client
+	log.Println("mongo connect success")
+}
+
+func closeMongo() {
+	if err := mc.Disconnect(context.Background()); err != nil {
+		log.Fatalf("mongo disconnect error: %v", err)
+	}
+}
+
+type Inventory struct {
+	ID   primitive.ObjectID `json:"id" bson:"_id"`
+	Item string             `json:"item" bson:"item"`
+	Qty  int                `json:"qty" bson:"qty"`
+	Tags []string           `json:"tags" bson:"tags"`
+	Size *Size              `json:"size" bson:"size"`
+}
+
+type Size struct {
+	H   int     `json:"h"`
+	W   float64 `json:"w"`
+	Uom string  `json:"uom"`
+}
+
+func TestQuickStart(t *testing.T) {
+	initMongo()
+	defer closeMongo()
+
+	// insert
+	coll := mc.Database("test").Collection("inventory")
+	inventory := &Inventory{
+		ID:   primitive.NewObjectID(),
+		Item: "canvas",
+		Qty:  100,
+		Tags: []string{"cotton"},
+		Size: &Size{
+			H:   28,
+			W:   35.5,
+			Uom: "cm",
+		},
+	}
+	idStr := inventory.ID.Hex()
+	log.Printf("mongo insertOne before, id: %s", idStr)
+	insertResult, err := coll.InsertOne(context.Background(), inventory)
+	if err != nil {
+		log.Fatalf("mongo insertOne error: %v", err)
+	}
+	log.Printf("mongo insertOne success, result: %v", insertResult)
+
+	// find
+	id, err := primitive.ObjectIDFromHex(idStr)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	filter := bson.M{"_id": id}
+	var in Inventory
+	if err = coll.FindOne(context.Background(), filter).Decode(&in); err != nil {
+		log.Fatalf("mongo findOne error: %v", err)
+	}
+	log.Printf("mongo findOne success, result: %v", in)
+
+	// update
+	update := bson.M{"$set": bson.M{"qty": 300}}
+	updateResult, err := coll.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatalf("mongo updateOne error: %v", err)
+	}
+	log.Printf("mongo updateOne success, result: %v", updateResult)
+
+	// delete
+	deleteResult, err := coll.DeleteOne(context.Background(), filter)
+	if err != nil {
+		log.Fatalf("mongo deleteOne error: %v", err)
+	}
+	log.Printf("mongo deleteOne success, result: %v", deleteResult)
+
+	// Output:
+	// Without monitor
+	// 2023/09/30 12:17:47 mongo connect success
+	// 2023/09/30 12:17:47 mongo insertOne before, id: 6517a16b3055dffe5d84c080
+	// 2023/09/30 12:17:47 mongo insertOne success, result: &{ObjectID("6517a16b3055dffe5d84c080")}
+	// 2023/09/30 12:17:47 mongo findOne success, result: {ObjectID("6517a16b3055dffe5d84c080") canvas 100 [cotton] 0x14000090100}
+	// 2023/09/30 12:17:47 mongo updateOne success, result: &{1 1 0 <nil>}
+	// 2023/09/30 12:17:47 mongo deleteOne success, result: &{1}
+
+	// With monitor
+	// [mongo] [info]  2023/09/30 12:38:51.225478 [CMD] {"ping": {"$numberInt":"1"},"lsid": {"id": {"$binary":{"base64":"winKRDFEQEGx4ycpFiyYkw==","subType":"04"}}},"$db": "admin"}
+	// 2023/09/30 12:38:51 mongo connect success
+	// 2023/09/30 12:38:51 mongo insertOne before, id: 6517a65baf830321b1b4a090
+	// [mongo] [info]  2023/09/30 12:38:51.226799 [CMD] {"insert": "inventory","ordered": true,"lsid": {"id": {"$binary":{"base64":"winKRDFEQEGx4ycpFiyYkw==","subType":"04"}}},"$db": "test","documents": [{"_id": {"$oid":"6517a65baf830321b1b4a090"},"item": "canvas","qty": {"$numberInt":"100"},"tags": ["cotton"],"size": {"h": {"$numberInt":"28"},"w": {"$numberDouble":"35.5"},"uom": "cm"}}]}
+	// 2023/09/30 12:38:51 mongo insertOne success, result: &{ObjectID("6517a65baf830321b1b4a090")}
+	// [mongo] [info]  2023/09/30 12:38:51.228045 [CMD] {"find": "inventory","filter": {"_id": {"$oid":"6517a65baf830321b1b4a090"}},"limit": {"$numberLong":"1"},"singleBatch": true,"lsid": {"id": {"$binary":{"base64":"winKRDFEQEGx4ycpFiyYkw==","subType":"04"}}},"$db": "test"}
+	// 2023/09/30 12:38:51 mongo findOne success, result: {ObjectID("6517a65baf830321b1b4a090") canvas 100 [cotton] 0x14000294180}
+	// [mongo] [info]  2023/09/30 12:38:51.229451 [CMD] {"update": "inventory","ordered": true,"lsid": {"id": {"$binary":{"base64":"winKRDFEQEGx4ycpFiyYkw==","subType":"04"}}},"$db": "test","updates": [{"q": {"_id": {"$oid":"6517a65baf830321b1b4a090"}},"u": {"$set": {"qty": {"$numberInt":"300"}}}}]}
+	// 2023/09/30 12:38:51 mongo updateOne success, result: &{1 1 0 <nil>}
+	// [mongo] [info]  2023/09/30 12:38:51.23098 [CMD] {"delete": "inventory","ordered": true,"lsid": {"id": {"$binary":{"base64":"winKRDFEQEGx4ycpFiyYkw==","subType":"04"}}},"$db": "test","deletes": [{"q": {"_id": {"$oid":"6517a65baf830321b1b4a090"}},"limit": {"$numberInt":"1"}}]}
+	// 2023/09/30 12:38:51 mongo deleteOne success, result: &{1}
+	// [mongo] [info]  2023/09/30 12:38:51.232269 [CMD] {"endSessions": [{"id": {"$binary":{"base64":"winKRDFEQEGx4ycpFiyYkw==","subType":"04"}}}],"$db": "admin"}
+}
