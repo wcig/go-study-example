941f82db9479fcb37131a556911a879d08aa41d7
wcig <job.yangbo@gmail.com>
2023-11-14 17:57:49 +0800

    study: concurrent cyclic barrier

--- study/concurrent/cyclic_barrier/cyclic_barrier_test.go
+++ study/concurrent/cyclic_barrier/cyclic_barrier_test.go
@@ -34,3 +34,33 @@ func TestCyclicBarrierDemo(t *testing.T) {
 	// 2023/11/14 17:48:20 >> 0 end
 	// 2023/11/14 17:48:22 >> over
 }
+
+func TestCyclicBarrierDemoWithAcion(t *testing.T) {
+	const num = 3
+	cnt := 0
+	barrier := cyclicbarrier.NewWithAction(num, func() error {
+		cnt++
+		return nil
+	})
+	for i := 0; i < num; i++ {
+		id := i
+		go func() {
+			log.Printf(">> %d start", id)
+			time.Sleep(time.Second)
+			if err := barrier.Await(context.Background()); err != nil {
+				log.Fatalf(">> %d err: %v", id, err)
+			}
+			log.Printf(">> %d end", id)
+		}()
+	}
+	time.Sleep(3 * time.Second)
+	log.Println(">> over", cnt)
+	// Output:
+	// 2023/11/14 17:57:26 >> 1 start
+	// 2023/11/14 17:57:26 >> 2 start
+	// 2023/11/14 17:57:26 >> 0 start
+	// 2023/11/14 17:57:27 >> 2 end
+	// 2023/11/14 17:57:27 >> 1 end
+	// 2023/11/14 17:57:27 >> 0 end
+	// 2023/11/14 17:57:29 >> over 1
+}
