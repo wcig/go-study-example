0e3c497cf0737432076940d0a577efad0499e01d
wcig <job.yangbo@gmail.com>
2023-12-01 22:18:01 +0800

    third: etcd locker

---
+++ third/etcd/locker/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"math/rand"
+	"time"
+
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"go.etcd.io/etcd/client/v3/concurrency"
+)
+
+var (
+	addr     = flag.String("addr", "http://localhost:2379", "etcd addresses")
+	lockName = flag.String("name", "my-test-lock", "lock name")
+)
+
+func main() {
+	// 创建client
+	ec, err := clientv3.New(clientv3.Config{Endpoints: []string{*addr}})
+	if err != nil {
+		log.Fatalf("init etcd client err: %v", err)
+	}
+	defer func() {
+		if err = ec.Close(); err != nil {
+			log.Fatalf("close etcd client err: %v", err)
+		}
+	}()
+
+	// 创建session
+	ss, err := concurrency.NewSession(ec)
+	if err != nil {
+		log.Fatalf("init etcd session err: %v", err)
+	}
+	defer func() {
+		if err = ss.Close(); err != nil {
+			log.Fatalf("close etcd session err: %v", err)
+		}
+	}()
+
+	// 创建锁
+	locker := concurrency.NewLocker(ss, *lockName)
+
+	// 加锁
+	log.Println("acquiring etcd lock")
+	locker.Lock()
+	log.Println("acquired etcd lock")
+
+	// 解锁
+	time.Sleep(time.Duration(rand.Intn(30)) * time.Second)
+	locker.Unlock()
+	log.Println("released etcd lock")
+}
