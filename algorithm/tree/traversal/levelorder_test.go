d2bd909b312ff09452e556f2f1eccec4f50f926c
wcig <job.yangbo@gmail.com>
2023-12-04 16:01:45 +0800

    algorithm: tree traversal

--- algorithm/tree/traversal/levelorder_test.go
+++ algorithm/tree/traversal/levelorder_test.go
@@ -32,7 +32,7 @@ func levelOrder(root *TreeNode, s *[]int) {
 	}
 }
 
-// 层序遍历,获取每层数组
+// 层序遍历,获取每层数组 (递归)
 func TestLevelOrderSlice(t *testing.T) {
 	root := getRoot()
 	result := [][]int{}
@@ -51,4 +51,36 @@ func levelOrderSlice(node *TreeNode, s *[][]int, level int) {
 	}
 	levelOrderSlice(node.Left, s, level+1)
 	levelOrderSlice(node.Right, s, level+1)
+}
+
+// 层序遍历,获取每层数组 (辅助队列)
+func TestLevelOrderSlice2(t *testing.T) {
+	root := getRoot()
+	result := [][]int{}
+	levelOrderSlice2(root, &result)
+	fmt.Println(">> levelOrderSlice2:", result) // >> levelOrderSlice: [[1] [2 3] [4 5 6 7]]
+}
+
+func levelOrderSlice2(root *TreeNode, s *[][]int) {
+	if root == nil {
+		return
+	}
+	var tmpArr []int
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		size := len(queue)
+		for i := 0; i < size; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			tmpArr = append(tmpArr, node.Val)
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		*s = append(*s, tmpArr)
+		tmpArr = []int{}
+	}
 }
