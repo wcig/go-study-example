2887f592fda99c394a90c8d96f44fcbe8d31cac6
wcig <job.yangbo@gmail.com>
2023-12-27 22:59:28 +0800

    other: go mod

--- book/100gomistakes/ch11_testing/90_testing_features/different_package/main_test.go
+++ book/100gomistakes/ch11_testing/90_testing_features/different_package/main_test.go
@@ -1,13 +1,11 @@
-package counter_test
+package counter
 
 import (
 	"testing"
-
-	counter "github.com/teivah/100-go-mistakes/11-testing/90-testing-features/different-package"
 )
 
 func TestCount(t *testing.T) {
-	if counter.Inc() != 1 {
+	if Inc() != 1 {
 		t.Errorf("expected 1")
 	}
 }
