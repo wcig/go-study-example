67f78ed984a8423da3976587803220525d69a25f
wcig <yangbo.work@outlook.com>
2024-07-06 23:17:23 +0800

    book: Go Web Programming

--- book/go_web_programming/chapter3/p34_https/main.go
+++ book/go_web_programming/chapter3/p34_https/main.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-// https服务：cert.pem为ssl证书，key.pem为服务器私钥
+// https服务：cert.pem为ssl证书，key.pem为证书对应私钥
 func main() {
 	server := http.Server{
 		Addr:    "127.0.0.1:28080",
